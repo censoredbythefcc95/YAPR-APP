af8356e06af3dbb78f0225fdf498eecd5186e60c
Steven Young <sastudillo6@gmail.com>
2023-11-26 16:56:01 -0500

    Implemented bcrypt into user password and post new user is now registered.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 const { check, validationResult } = require('express-validator'); // Validator-check is deprecated - you need to call express-validator directly instead.
 
 // @route  POST api/users
@@ -12,13 +15,48 @@ router.post('/', [
     check('email', 'Please include a valid email address.').isEmail(),
     check('password', 'Please enter a password with at least 6 characters.').isLength({ min: 6 })
     ], 
-    (req, res) => {
+    async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send('This is the YAPR User Route.');
-})
+    
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            // Nodemon was crashing when this was returning. Added return statement to avoid headers being sent again and again.
+            return res.status(400).json({ errors: [{ msg: "User already exists!"}] });
+        }
+
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        //Return webtoken
+    res.send('User has been successfully registered.');
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('There was a server error.');
+    }
+});
 
 
 module.exports = router;
