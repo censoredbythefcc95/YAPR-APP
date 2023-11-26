2bf6420f6b4da3faa8f4e5b072bfdcd5e8266aae
Steven Young <sastudillo6@gmail.com>
2023-11-26 16:01:54 -0500

    Logic for express-validation working on User route.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator'); // Validator-check is deprecated - you need to call express-validator directly instead.
 
-// @route  GET api/users
-// @desc   This will test the route.
+// @route  POST api/users
+// @desc   This will register a new user into the database.
 // @access Public route 
-router.get('/', (req, res) => res.send('This is the YAPR User route.'));
+router.post('/', [
+    check ('name', 'Missing name in name field.')
+    .not()
+    .isEmpty(),
+    check('email', 'Please include a valid email address.').isEmail(),
+    check('password', 'Please enter a password with at least 6 characters.').isLength({ min: 6 })
+    ], 
+    (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('This is the YAPR User Route.');
+})
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connecting to datbase
 connectDB();
 
+// Initial middleware for body parser.
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
     res.send('The YAPR API is now running at this time.');
 })
