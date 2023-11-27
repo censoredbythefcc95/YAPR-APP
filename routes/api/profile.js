54b183ffa10485335b07e83bb06a35ac4b27e094
Steven Young <sastudillo6@gmail.com>
2023-11-26 23:43:07 -0500

    Profile route get api profile me set.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
 
-// @route  GET api/profile
-// @desc   This will test the route.
-// @access Public route 
-router.get('/', (req, res) => res.send('This is the YAPR profile route.'));
+// @route  GET api/profile/me
+// @desc   Get current users profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user.'}); //Adding return to avoid HTTP HEADER ERROR
+        }
+        console.log('Profile acquired.');
+        res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('There was a server error.');
+    }
+});
 
 module.exports = router;
