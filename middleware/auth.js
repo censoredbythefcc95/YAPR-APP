e0bda193f835f70655cf3116e226659791398a55
Steven Young <sastudillo6@gmail.com>
2023-11-26 23:07:04 -0500

    Bearer token auth route updated and functional.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
+// adding console logs to each parameter of function to troubleshoot.
 module.exports = function(req, res, next) {
     //Get token from the header
     const token = req.header('x-auth-token');
 
     //check if not token or not found
     if (!token) {
+        console.log('Denied');
         return res.status(401).json({ message: 'No token - authorization denied.'});
+        
     }
 
     // Verify token and match to user
@@ -15,8 +18,10 @@ module.exports = function(req, res, next) {
         const decoded = jwt.verify(token, config.get('jwtSecret'));
 
         req.user = decoded.user;
+        console.log('Decoded');
         next();
     } catch(err) {
         res.status(401).json({msg: 'Token is not valid'});
+        console.log('Invalid');
     }
 }
